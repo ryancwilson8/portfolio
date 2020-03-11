f3b41ccd76ab6c7aa3a647c6d0252efb88bb2c71
Ryan Wilson <Ryan@Ryans-MacBook-Pro.local>
2020-03-11 08:12:43 +0000

    Refactored app js to become component class.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/header/header.react";
 import Caret from "./components/caret/caret.react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <Caret />
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = { toggleCaret : false }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+        this.setState({ toggleCaret: true })
+      }, 10000);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header />
+          {this.state.toggleCaret ? <Caret /> : null }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/caret/caret.react.js
+++ src/components/caret/caret.react.js
@@ -3,12 +3,11 @@ import Reveal from "react-reveal/Reveal";
 import "./caret.css";
 
 class Caret extends Component {
+
   render() {
     return (
       <div className="caret_wrapper">
-        <Reveal effect="fadeInUp" delay={1500} duration={3000}>
-          <p className="bigCaret">V</p>
-        </Reveal>
+        <p className="bigCaret">V</p>
       </div>
     );
   }
